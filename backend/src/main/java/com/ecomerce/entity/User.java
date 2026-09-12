1bbdd2dd5960fb01f85347565c15a34e97a76b1a
Gilson Ramos <gilsrms0@gmail.com>
2026-09-12 09:35:13 -0300

    feat(backend): create User entity

---
+++ backend/src/main/java/com/ecomerce/entity/User.java
@@ -0,0 +1,41 @@
+package com.ecomerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false,unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password_hash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
